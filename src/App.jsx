7c311729e665240d3a01a948d72fbc2e17c66468
João Leodoro <joaooleodoro@gmail.com>
2024-10-20 21:09:09 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/header"
 import Frase from "./sections/Frase"
-import Cartaoideal from "./sections/cartaoideal"
+import Cartaoideal from "./sections/Cartaoideal"
 import Herosection from "./sections/herosection"
 import Ultrablue from "./sections/ultrablue"
 
